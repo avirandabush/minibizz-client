92f63551e71c281426f6b108cd2e253bda9da693
Aviran Dabush <avirandabush@gmail.com>
2026-03-31 11:38:14 +0300

    use env for base url.

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -1,6 +1,6 @@
 import { auth } from '../lib/firebase'
 
-const BASE_URL = 'https://minibizz-server.onrender.com'
+const BASE_URL = import.meta.env.VITE_API_URL
 
 async function getAuthToken() {
   const user = auth.currentUser
